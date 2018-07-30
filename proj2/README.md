8088071dea4cce79ccf8ee0f890bb09684e57b0f
stefano l <stefan.emailacct@gmail.com>
2018-07-31 00:42:35 +0100

    k

--- proj2/README.md
+++ proj2/README.md
@@ -25,3 +25,13 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+
+> to select with from which module to generate:
+
+```
+    ex:
+    ng g component mainMenu --module modules/app.module
+
+    cause i'm using material as its own module.
+```
--- proj2/src/app/modules/app.module.ts
+++ proj2/src/app/modules/app.module.ts
@@ -1,21 +1,23 @@
+import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from './material.module';
-
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-
 
 import 'hammerjs';
 
-
 import { AppComponent } from '../app.component';
+import { MainMenuComponent } from '../main-menu/main-menu.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent, 
+    MainMenuComponent
+  ],
+
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
