07d73afc2caa2c921853ba26600bb1c061d8ca9f
stefano l <stefan.emailacct@gmail.com>
2018-08-08 16:51:05 +0100

    need to look into angular-flex-layout

--- proj2/src/app/app.module.ts
+++ proj2/src/app/app.module.ts
@@ -4,23 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './modules/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
 
 //components
-import { AppComponent } from '../app.component';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
-import { RandomSeasonsComponent } from '../random-seasons/random-seasons.component';
-import { EpisodeDetailsComponent } from '../random-seasons/episode-details/episode-details.component';
-import { Season10Component } from '../season10/season10.component';
-import { Season10DetailsComponent } from '../season10/details/details.component';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RandomSeasonsComponent } from './random-seasons/random-seasons.component';
+import { EpisodeDetailsComponent } from './random-seasons/episode-details/episode-details.component';
+import { Season10Component } from './season10/season10.component';
+import { Season10DetailsComponent } from './season10/details/details.component';
 
 //services
-import { EpisodeDataService } from '../services/episode-data.service';
-import { Season10DataService } from '../services/season10-data.service';
+import { EpisodeDataService } from './services/episode-data.service';
+import { Season10DataService } from './services/season10-data.service';
+
 
 
 
